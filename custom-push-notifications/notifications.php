85dc89575c8bf0a22e1c81dd8e5778b60774f34b
alexlampe14 <lampealex888@gmail.com>
2024-03-07 20:46:02 -0500

    Create notifications.php

---
+++ custom-push-notifications/notifications.php
@@ -0,0 +1,12 @@
+/*
+Plugin Name:  Custom Push Notifications
+Plugin URI:   https://wordpress.com/
+Description:  Display real-time push notifications. 
+Version:      1.0
+Author:       Wafik Tawfik, 
+Author URI:   https://wordpress.com/
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  wpb-tutorial
+Domain Path:  /languages
+*/
